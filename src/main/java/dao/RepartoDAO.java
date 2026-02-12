58edbe0f27e21b04f5833d648e6e3ea60cd6888a
Antonio Jose Moral Correa <tonyjorlviking@gmail.com>
2026-02-12 20:16:10 +0100

    crear las clases de daos de suscripcion y reparto

--- src/main/java/dao/RepartoDAO.java
+++ src/main/java/dao/RepartoDAO.java
@@ -4,10 +4,48 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import models.Reparto;
+
 /**
  *
  * @author User
  */
-public class RepartoDAO {
+public class RepartoDAO extends DAO<Reparto>{
+
+    //Atributos
+    private static final String INSERT = "INSERT INTO reparto (id_filmografia, nombre_actor, papel) VALUES (?,?,?)";
+    private static final String DELETE = "DELETE FROM reparto WHERE id = ?";
+    private static final String UPDATE = "UPDATE reparto SET id_filmografia = ?, nombre_actor = ?, papel = ?, WHERE id = ?";
+    private static final String LISTALL = "SELECT *  FROM reparto";
+    private static final String LISTONE = "SELECT *  FROM reparto WHERE id = ?";
+    
+    //Constructor
+    public RepartoDAO(Connection conexion) {
+        super(conexion);
+    }
+
+    //Metodos
+    @Override
+    public List<Reparto> listAll() throws SQLException {
+    }
+
+    @Override
+    public Reparto listOne(int id) throws SQLException {
+    }
+
+    @Override
+    public void insertar(Reparto t) throws SQLException {
+    }
+
+    @Override
+    public void actualizar(Reparto t, int id) throws SQLException {
+    }
+
+    @Override
+    public void eliminar(int id) throws SQLException {
+    }
     
 }
--- src/main/java/dao/SuscripcionDAO.java
+++ src/main/java/dao/SuscripcionDAO.java
@@ -4,10 +4,47 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import models.Suscripcion;
+
 /**
  *
  * @author User
  */
-public class SuscripcionDAO {
+public class SuscripcionDAO extends DAO<Suscripcion>{
+
+    //Atributos
+    private static final String INSERT = "INSERT INTO suscripcion ( cuenta_id, tipo_id, fecha_contratacion,  fecha_fin) VALUES (?,?,?,?)";
+    private static final String DELETE = "DELETE FROM suscripcion WHERE id = ?";
+    private static final String UPDATE = "UPDATE suscripcion SET  cuenta_id = ?, tipo_id = ?, fecha_contratacion = ?, fecha_fin = ? WHERE id = ?";
+    private static final String LISTALL = "SELECT *  FROM suscripcion";
+    private static final String LISTONE = "SELECT *  FROM suscripcion WHERE id = ?";
+    //Constructor
+    public SuscripcionDAO(Connection conexion) {
+        super(conexion);
+    }
+
+    //Metodos
+    @Override
+    public List<Suscripcion> listAll() throws SQLException {
+    }
+
+    @Override
+    public Suscripcion listOne(int id) throws SQLException {
+    }
+
+    @Override
+    public void insertar(Suscripcion t) throws SQLException {
+    }
+
+    @Override
+    public void actualizar(Suscripcion t, int id) throws SQLException {
+    }
+
+    @Override
+    public void eliminar(int id) throws SQLException {
+    }
     
 }
